2f06e15ac9bff3fb962bbabdcb07b0bffdc51ea0
nickiduval <103085467+nickiduval@users.noreply.github.com>
2022-04-19 22:10:08 -0400

    new website stuff

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/*
+* Author: Nicki Duval
+* Created: 19 April 2022
+* License: Public Domain
+*/
+
+// Variables
+var make = "Toyota";
+var model = "Prius";
+var color = "Silver";
+var year = 2005;
+var ownIt = true;
+
+// Calculate
+var age = 2022 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Own it? " + ownIt + "<br>");
